d5aaebd20800e8119dbeedd331062e5c3a01849f
teacrepe <crepeteaiy@gmail.com>
2023-05-10 22:01:49 +0900

    feat: update git handling module: git_handler.py(#5)

--- QFilemanager/git_handler.py
+++ QFilemanager/git_handler.py
@@ -3,47 +3,64 @@ import os
 def git_status(dir_path):
     os.chdir(dir_path)
     result = os.popen('git status').read()
-    return result
+    print('result: ' + result)
+    if result == '':
+        return False
+    return True
 
 def git_init(dir_path):
     os.chdir(dir_path)
     result = os.popen('git init').read()
-    return result
+    if "fatal: " in result:
+        return False
+    return True
 
 def git_add(dir_path, file_name):
     os.chdir(dir_path)
     result = os.popen('git add ' + file_name).read()
-    return result
+    return True
 
 def git_restore(dir_path, file_name):
     os.chdir(dir_path)
     result = os.popen('git restore ' + file_name).read()
-    return result
+    if "fatal: " in result:
+        return False
+    return True
 
 def git_restore_staged(dir_path, file_name):
     os.chdir(dir_path)
     result = os.popen('git restore --staged ' + file_name).read()
-    return result
+    if "fatal: " in result:
+        return False
+    return True
 
 def git_untrack(dir_path, file_name):
     os.chdir(dir_path)
     result = os.popen('git rm --cached ' + file_name).read()
-    return result
+    if "fatal: " in result:
+        return False
+    return True
 
 def git_rm(dir_path, file_name):
     os.chdir(dir_path)
     result = os.popen('git rm ' + file_name).read()
-    return result
+    if "fatal: " in result:
+        return False
+    return True
 
 def git_mv(dir_path, file_name, new_name):
     os.chdir(dir_path)
     result = os.popen('git mv ' + file_name + ' ' + new_name).read()
-    return result
+    if "fatal: " in result:
+        return False
+    return True
 
 def git_commit(dir_path, commit_msg):
     os.chdir(dir_path)
     result = os.popen('git commit -m "' + commit_msg + '"').read()
-    return result
+    if "fatal: " in result:
+        return False
+    return True
 
 def get_status_list(dir_path):
     os.chdir(dir_path)
