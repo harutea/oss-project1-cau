9eb3b49a9ecc867d1b65c63dabdf09e28c93327b
teacrepe <crepeteaiy@gmail.com>
2023-06-12 17:06:03 +0900

    fix: correct error in git_clone in git_handler.py

--- QFilemanager/git_handler.py
+++ QFilemanager/git_handler.py
@@ -242,6 +242,7 @@ def git_clone(dir_path, branch_name, id, token, url):
     password = f"{token}"
     repo_name = url.split("//")[-1]
     repo_name_final = repo_name.split('/')[-1]
+    repo_name_final = repo_name_final.split('.')[0]
     remote = f"https://{username}:{password}@{repo_name}"
     print("remote : " + remote)
 
@@ -250,7 +251,7 @@ def git_clone(dir_path, branch_name, id, token, url):
     #os.popen('git clone '+'https://' + id +
     #         '@' + repo_name)
     if 'Repository not found.' in os.popen('git ls-remote '+ remote).read():
-        Repo.clone_from(remote, dir_path + repo_name_final)
+        Repo.clone_from(remote, dir_path + '/' + repo_name_final)
         return True
     else:
         return False
